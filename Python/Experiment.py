855e98fc2a6f51e0ff4776dfd20c9a55d423a876
Jahanzaibali2 <jahanzaibalikhawaja26@gmail.com>
2024-11-24 15:34:21 +0500

    Want to show my code to AbdurRehman

--- Python/Experiment.py
+++ Python/Experiment.py
@@ -3,3 +3,14 @@ x = 7/3
 y = 7//3
 print(x)
 print(y)
+
+
+yearsOfService = int(input("Enter your year's of service: "))
+Salary = int(input("Enter your Salary: "))
+
+if yearsOfService >= 5:
+    Salary = Salary*0.05
+    print(f"Your recieved a 5% bonus, your salary for this month is {Salary}")
+
+else:
+    print("You're not eligible")
